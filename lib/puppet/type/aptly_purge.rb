5020b9c1c138a92e223873b70b2b804d85342692
Josh Snyder <josh@code406.com>
2015-09-15 00:57:20 -0700

    Add a sanity check to ensure all managed packages are installed

--- lib/puppet/type/aptly_purge.rb
+++ lib/puppet/type/aptly_purge.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'open3'
 
 Puppet::Type.newtype(:aptly_purge) do
   @doc = <<-EOD
@@ -22,8 +23,23 @@ EOD
   end
 
   def generate
-    (managed_packages, unmanaged_packages) = Puppet::Type.type('package').instances.select do |p|
-      ['apt', 'aptitude'].include?(p.provider.class.name.to_s)
+    outfile = "/dev/stdout"
+    package = Puppet::Type.type(:package)
+
+    unless all_packages_synced
+      warning <<EOS
+
+It isn't safe to purge packages right now, because there are packages in the
+catalog that aren't synced on the system. Package purging is skipped for this
+Puppet run.
+EOS
+      raise Puppet::Error.new("Could not purge packages during this Puppet run")
+    end
+
+    # Using the RAL, divide the world into Catalog packages and not-Catalog
+    # packages.
+    (managed_packages, unmanaged_packages) = package.instances.select do |p|
+      p.provider.is_a?(Puppet::Type::Package::ProviderDpkg)
     end.partition { |r| catalog.resource_refs.include? r.ref }
 
     managed_package_names = managed_packages.map(&:name)
@@ -36,36 +52,17 @@ EOD
     # B is marked as 'auto' as it should
     # If some other process has marked A as auto, B will get ensure=>absent
     # Then dpkg will remove both A and B.  This is bad!
-    Open3.pipeline_w('xargs apt-mark manual') do |i, ts|
-      i.puts(managed_packages.map(&:name))
-      i.close
-      ts[0].value.success? or raise "Failed to apt-mark packages"
-    end
+    mark_manual managed_package_names, outfile
 
     # It would be excellent to set 'apt-mark hold' on all managed packages
     # here, but it turns out this doesn't interact well with dpkg based
     # package providers.
 
-    Open3.pipeline_w('xargs apt-mark auto') do |i, ts|
-      i.puts(unmanaged_package_names)
-      i.close
-      ts[0].value.success? or raise "Failed to apt-mark packages"
-    end
+    mark_auto unmanaged_package_names, outfile
 
-    Open3.pipeline_w('xargs apt-mark unhold') do |i, ts|
-      i.puts(unmanaged_package_names)
-      i.close
-      ts[0].value.success? or raise "Failed to apt-mark packages"
-    end
+    unhold unmanaged_package_names, outfile
 
-    apt_would_purge = Open3.pipeline_r('apt-get -s autoremove') do |i, ts|
-      p = i.each_line.map {|line|
-        match = /^Purg (\S*)/.match(line)
-	match[1] if match
-      }.compact.to_set
-      ts[0].value.success? or raise "Failed to simulate apt-get autoremove"
-      p
-    end
+    apt_would_purge = get_purges()
 
     unmanaged_packages.select do |r|
       # This is the crux.  We intersect the list of packages Puppet isn't
@@ -78,6 +75,55 @@ EOD
       end
 
       resource.purging
+    end
+  end
+
+  private
+
+  def all_packages_synced
+    package = Puppet::Type.type(:package)
+    catalog.resources.find_all do |r|
+      r.is_a?(package) and
+      r.provider.is_a?(Puppet::Type::Package::ProviderDpkg)
+    end.all? do |r|
+      ensure_p = r.parameters[:ensure]
+      is = ensure_p.retrieve
+      ensure_p.insync?(is)
+    end
+  end
+
+  def mark_manual(packages, outfile)
+    Open3.pipeline_w('xargs apt-mark manual', :out=>outfile) do |i, ts|
+      i.puts(packages)
+      i.close
+      ts[0].value.success? or raise "Failed to apt-mark packages"
+    end
+  end
+
+  def mark_auto(packages, outfile)
+    Open3.pipeline_w('xargs apt-mark auto', :out=>outfile) do |i, ts|
+      i.puts(packages)
+      i.close
+      ts[0].value.success? or raise "Failed to apt-mark packages"
+    end
+  end
+
+  def unhold(packages, outfile)
+    Open3.pipeline_w('xargs apt-mark unhold', :out=>outfile) do |i, ts|
+      i.puts(packages)
+      i.close
+      ts[0].value.success? or raise "Failed to apt-mark packages"
+    end
+  end
+
+  def get_purges
+    Open3.pipeline_r('apt-get -s autoremove') do |i, ts|
+      p = i.each_line.map {|line|
+        match = /^Purg (\S*)/.match(line)
+        match[1] if match
+      }.compact.to_set
+      ts[0].value.success? or raise "Failed to simulate apt-get autoremove"
+      p
     end
   end
 end
